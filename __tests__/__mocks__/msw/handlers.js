cea61849aee89c2104108e0ea373a4ee305afe0b
Choi Eun Jeong <qhdrn67@naver.com>
2023-08-16 22:56:35 +0900

    ui testing: reservation component w/ msw

--- __tests__/__mocks__/msw/handlers.js
+++ __tests__/__mocks__/msw/handlers.js
@@ -1,1 +1,16 @@
-export const handlers = [];
+import { rest } from 'msw';
+import { readFakeData } from '../fakeData';
+
+const reservationHandler = rest.get(
+  'http://localhost:3000/api/shows/:showId',
+  async (req, res, ctx) => {
+    const { fakeShows } = await readFakeData();
+    return res(
+      ctx.json({
+        show: fakeShows[0],
+      })
+    );
+  }
+);
+
+export const handlers = [reservationHandler];
