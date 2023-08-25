d3db58727f32bf76f2100bfd338388d6f2b73cc9
Choi Eun Jeong <qhdrn67@naver.com>
2023-08-26 00:02:44 +0900

    test: revalidate ISR cache #2

--- cypress/e2e/isr-revalidate.cy.js
+++ cypress/e2e/isr-revalidate.cy.js
@@ -1,4 +1,5 @@
 import { generateNewBand } from '@/__tests__/__mocks__/fakeData/newBand';
+import { generateNewShow } from '@/__tests__/__mocks__/fakeData/newShow';
 import { generateRandomId } from '@/lib/features/reservations/utils';
 
 it('should load refreshed page from cache after new band is added', () => {
@@ -24,5 +25,27 @@ it('should load refreshed page from cache after new band is added', () => {
   cy.findByRole('heading', { name: /avalanche of cheese/i }).should('exist');
 
   // reset ISR cache to initial db conditions
+  cy.resetDbAndIsrCache();
+});
+
+it('should load refreshed page from cache after new show is added', () => {
+  cy.task('resetDB').visit('/shows');
+  cy.findByRole('heading', { name: /avalanche of cheese/i }).should(
+    'not.exist'
+  );
+
+  const showId = generateRandomId();
+  const newShow = generateNewShow(showId);
+  const secret = Cypress.env('REVALIDATION_SECRET');
+
+  cy.request('POST', `/api/shows?secret=${secret}`, { newShow }).then(
+    (response) => {
+      expect(response.body.revalidated).to.equal(true);
+    }
+  );
+
+  cy.reload();
+  cy.findByRole('heading', { name: /avalanche of cheese/i }).should('exist');
+
   cy.resetDbAndIsrCache();
 });
