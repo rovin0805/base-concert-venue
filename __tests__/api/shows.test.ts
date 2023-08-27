e349114970924adedbf0f0d59f36b5647827757a
Choi Eun Jeong <qhdrn67@naver.com>
2023-08-27 15:36:42 +0900

    test: a route with a url params

--- __tests__/api/shows.test.ts
+++ __tests__/api/shows.test.ts
@@ -1,9 +1,10 @@
 /* File: test/unit.test.ts */
 import { testApiHandler } from 'next-test-api-route-handler';
-import showsHandler from '@/pages/api/shows';
 import { readFakeData } from '@/__tests__/__mocks__/fakeData';
+import showsHandler from '@/pages/api/shows';
+import showIdHandler from '@/pages/api/shows/[showId]';
 
-test('/api/shows returns shows from db', async () => {
+test('GET /api/shows returns shows from db', async () => {
   await testApiHandler({
     handler: showsHandler,
     test: async ({ fetch }) => {
@@ -12,6 +13,20 @@ test('/api/shows returns shows from db', async () => {
       const json = await res.json();
       const { fakeShows } = await readFakeData();
       expect(json).toEqual({ shows: fakeShows });
+    },
+  });
+});
+
+test('GET /api/shows/[showId] returns the data for the correct show ID ', async () => {
+  await testApiHandler({
+    handler: showIdHandler,
+    paramsPatcher: (params) => (params.showId = 0),
+    test: async ({ fetch }) => {
+      const res = await fetch({ method: 'GET' });
+      expect(res.status).toBe(200);
+      const json = await res.json();
+      const { fakeShows } = await readFakeData();
+      expect(json).toEqual({ show: fakeShows[0] });
     },
   });
 });
