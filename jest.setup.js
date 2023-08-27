5757bfb5b7803be18937dc3bcb13baf716f7874c
Choi Eun Jeong <qhdrn67@naver.com>
2023-08-27 15:02:37 +0900

    fix: polyfill, resetDB beforeEach, ignore db dir in watchlist

--- jest.setup.js
+++ jest.setup.js
@@ -5,10 +5,24 @@
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
 
+// polyfill necessary for jsdom test environment
+// reference: https://jestjs.io/docs/configuration#watchpathignorepatterns-arraystring
+import { TextDecoder, TextEncoder } from 'util';
+
 // src/setupTests.js
 import { server } from './__tests__/__mocks__/msw/server.js';
+
+import { resetDB } from './__tests__/__mocks__/db/utils/reset-db';
+
+global.TextDecoder = TextDecoder;
+global.TextEncoder = TextEncoder;
+
 // Establish API mocking before all tests.
 beforeAll(() => server.listen());
+
+beforeEach(async () => {
+  await resetDB();
+});
 
 // Reset any request handlers that we may add during the tests,
 // so they don't affect other tests.
