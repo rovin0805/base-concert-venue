d3e9c6fe413be8ccd81d1f78c13df06379df8f0e
Choi Eun Jeong <qhdrn67@naver.com>
2023-08-27 16:47:50 +0900

    test: post reservation api route

---
+++ __tests__/api/reservations.test.ts
@@ -0,0 +1,38 @@
+import { testApiHandler } from 'next-test-api-route-handler';
+import reservationHandler from '@/pages/api/reservations/[reservationId]';
+import userReservationsHandler from '@/pages/api/users/[userId]/reservations';
+
+jest.mock('@/lib/auth/utils');
+
+test('POST /api/reservations/[reservationId] create a reservation', async () => {
+  await testApiHandler({
+    handler: reservationHandler,
+    paramsPatcher: (params) => (params.reservationId = 12345),
+    test: async ({ fetch }) => {
+      const res = await fetch({
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          seatCount: 5,
+          userId: 1,
+          showId: 0,
+        }),
+      });
+      expect(res.status).toBe(201);
+    },
+  });
+
+  await testApiHandler({
+    handler: userReservationsHandler,
+    paramsPatcher: (params) => (params.userId = 1),
+    test: async ({ fetch }) => {
+      const res = await fetch({ method: 'GET' });
+      expect(res.status).toBe(200);
+
+      const json = await res.json();
+      expect(json.userReservations).toHaveLength(3);
+    },
+  });
+});
